af343fdfbd978bb7255c68caccf7490c5c0c3af0
Emma Edvardsson <emmed608@student.liu.se>
2015-03-15 00:12:49 +0100

    En graph

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// When pages is fully loaded- do this
+// When pages are fully loaded - do this
 $(document).ready(function() {
   document.getElementById("startView").style.display = "none";
   document.getElementById("mapView").style.display = "inherit";
@@ -7,9 +7,9 @@ $(document).ready(function() {
 // When click on enter
 $(document).keypress(function(e) {
     if(e.which == 13 && $("startView").is(":visible")) {
-        $( "#searchButton" ).click(); // Act like when serachButton is clicked
+        $( "#searchButton" ).click(); // Act like when searchButton is clicked
     }
     if(e.which == 13 && $("mapView").is(":visible")) {
-        $( "#control-panel-searchButton" ).click(); // Act like when serachButton is clicked
+        $( "#control-panel-searchButton" ).click(); // Act like when searchButton is clicked
     }
 });
